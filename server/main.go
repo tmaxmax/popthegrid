bf08727bca0b3277f5ef363c87d4c5d5992e6b15
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2020-12-03 00:32:40 +0200

    Create server application

---
+++ server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/Pallinder/go-randomdata"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use("/", func(c *fiber.Ctx) error {
+		if websocket.IsWebSocketUpgrade(c) {
+			c.Locals("allowed", true)
+			return c.Next()
+		}
+		return fiber.ErrUpgradeRequired
+	})
+
+	app.Get("/", websocket.New(func(c *websocket.Conn) {
+		for {
+			if err := c.WriteJSON(map[string]interface{}{
+				"name": randomdata.SillyName(),
+			}); err != nil {
+				log.Println("Write:", err)
+				break
+			}
+			time.Sleep(time.Second * 2)
+		}
+	}))
+
+	log.Fatalln(app.Listen(":80"))
+}
