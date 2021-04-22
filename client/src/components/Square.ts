1d74ed7110ffbce5b8f5e01977d89a4edc0726c2
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-22 20:17:00 +0300

    Remove computed props, unused setters, type event names, and use the component's promise-based event API

--- client/src/components/Square.ts
+++ client/src/components/Square.ts
@@ -3,7 +3,7 @@ import './Square.css'
 import { Component } from '../internal/Component'
 
 interface SquareEventListener {
-  event: string
+  event: keyof HTMLElementEventMap
   callback: (this: Square, event: Event) => void
   options?: boolean | AddEventListenerOptions
 }
@@ -13,7 +13,7 @@ interface SquareProperties {
   eventListeners?: SquareEventListener[]
 }
 
-class Square extends Component<HTMLDivElement> {
+class Square extends Component<HTMLDivElement, false> {
   private userEvents?: SquareEventListener[]
 
   constructor(properties: SquareProperties) {
@@ -24,60 +24,39 @@ class Square extends Component<HTMLDivElement> {
       this.setStyle('z-index', `${2}`)
     })
     this.addEventListener('mouseleave', () => {
-      const transitionEventHandler = function (this: Square) {
-        this.setStyle('z-index', '')
-        this.removeEventListener('transitionend', transitionEventHandler)
-      }.bind(this)
       this.setStyle('z-index', `${1}`)
-      this.addEventListener('transitionend', transitionEventHandler)
+      this.eventsRace(['transitionend', 'transitioncancel']).then(() => this.setStyle('z-index', null))
     })
-  }
-
-  get color(): string {
-    return this.getStyle('--color')
   }
 
   set color(color: string) {
     this.setStyle('--color', color)
   }
 
-  get row(): number {
-    return parseInt(this.getStyle('--row'), 10)
-  }
-
   set row(row: number) {
     this.setStyle('--row', `${row}`)
-  }
-
-  get col(): number {
-    return parseInt(this.getStyle('--col'), 10)
   }
 
   set col(col: number) {
     this.setStyle('--col', `${col}`)
   }
 
-  async create<T extends HTMLElement>(parent: Component<T>, animate: boolean): Promise<void> {
-    this.appendTo(parent)
+  animate(which: string, animate = true): Promise<void> {
     if (animate) {
-      this.addClass('grid__square--inserted')
-      await new Promise<void>((resolve) => {
-        this.addEventListener('animationend', () => {
-          this.removeClass('grid__square--inserted')
-          resolve()
-        })
-      })
+      const c = `grid__square--${which}`
+      this.addClass(c)
+      return this.eventsRace(['animationend', 'animationcancel']).then(() => this.removeClass(c))
     }
+    return Promise.resolve()
   }
 
-  async destroy(animate: boolean): Promise<void> {
-    if (animate) {
-      await new Promise<void>((resolve) => {
-        this.addEventListener('animationend', () => resolve())
-        this.addClass('grid__square--deleted')
-      })
-    }
-    this.remove()
+  create<V extends boolean>(parent: Component<HTMLElement, V>, animate: boolean): Promise<void> {
+    this.appendTo(parent)
+    return this.animate('inserted', animate)
+  }
+
+  destroy(animate: boolean): Promise<void> {
+    return this.animate('deleted', animate).then(() => this.remove())
   }
 }
 
