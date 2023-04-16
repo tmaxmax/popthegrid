aac8a14a3bc21d47455cb3ab16103d6f7e632713
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2023-04-16 21:17:51 +0300

    Compress migrations
    The database was only built in dev, no need for more migrations.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -12,29 +12,18 @@ const migrations: Migration[] = [
 
       store.createIndex('gamemode', 'gamemode', { unique: false })
       store.createIndex('isWin', 'isWin', { unique: false })
+      store.createIndex('startedAt', 'startedAt', { unique: false })
+      store.createIndex('duration', 'duration', { unique: false })
     },
     down(db, tx) {
       const store = tx.objectStore(ATTEMPTS_STORE)
 
       store.deleteIndex('gamemode')
       store.deleteIndex('isWin')
-      store.clear()
-
-      db.deleteObjectStore(ATTEMPTS_STORE)
-    },
-  },
-  {
-    up(_, tx) {
-      const store = tx.objectStore(ATTEMPTS_STORE)
-
-      store.createIndex('startedAt', 'startedAt', { unique: false })
-      store.createIndex('duration', 'duration', { unique: false })
-    },
-    down(_, tx) {
-      const store = tx.objectStore(ATTEMPTS_STORE)
-
       store.deleteIndex('startedAt')
       store.deleteIndex('duration')
+
+      db.deleteObjectStore(ATTEMPTS_STORE)
     },
   },
 ]
