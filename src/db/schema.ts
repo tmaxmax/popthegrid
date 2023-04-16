7cdb1d12fb2964db55fa318c96ed97d637f9490c
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2023-04-16 20:04:25 +0300

    Implement schema migrations

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,18 +1,48 @@
 import { OpenOptions } from '$util/indexedDB'
+import { Migration, createConfigurator } from '$util/indexedDB/schema'
 import { ATTEMPTS_STORE } from './attempt'
+
+const migrations: Migration[] = [
+  {
+    up(db) {
+      const store = db.createObjectStore(ATTEMPTS_STORE, {
+        keyPath: 'id',
+        autoIncrement: true,
+      })
+
+      store.createIndex('gamemode', 'gamemode', { unique: false })
+      store.createIndex('isWin', 'isWin', { unique: false })
+    },
+    down(db, tx) {
+      const store = tx.objectStore(ATTEMPTS_STORE)
+
+      store.deleteIndex('gamemode')
+      store.deleteIndex('isWin')
+      store.clear()
+
+      db.deleteObjectStore(ATTEMPTS_STORE)
+    },
+  },
+  {
+    up(_, tx) {
+      const store = tx.objectStore(ATTEMPTS_STORE)
+
+      store.createIndex('startedAt', 'startedAt', { unique: false })
+      store.createIndex('duration', 'duration', { unique: false })
+    },
+    down(_, tx) {
+      const store = tx.objectStore(ATTEMPTS_STORE)
+
+      store.deleteIndex('startedAt')
+      store.deleteIndex('duration')
+    },
+  },
+]
 
 const schema: OpenOptions = {
   name: 'popthegrid',
-  version: 1,
-  configurator({ database: db }) {
-    const store = db.createObjectStore(ATTEMPTS_STORE, {
-      keyPath: 'id',
-      autoIncrement: true,
-    })
-
-    store.createIndex('gamemode', 'gamemode', { unique: false })
-    store.createIndex('isWin', 'isWin', { unique: false })
-  },
+  version: migrations.length,
+  configurator: createConfigurator(migrations),
 }
 
 export default schema
