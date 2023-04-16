9b3af8927b3c688a6f00a6ed502029ad1bdc15fd
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2023-04-16 21:55:25 +0300

    Remove down migrations
    IndexedDB has no concept of 'down' migrations – one cannot open a
    database with a lower version than the existing one.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,28 +2,16 @@ import { Schema, Migration, createConfigurator } from '$util/indexedDB/schema'
 import { ATTEMPTS_STORE } from './attempt'
 
 const migrations: Migration[] = [
-  {
-    up(db) {
-      const store = db.createObjectStore(ATTEMPTS_STORE, {
-        keyPath: 'id',
-        autoIncrement: true,
-      })
+  (db) => {
+    const store = db.createObjectStore(ATTEMPTS_STORE, {
+      keyPath: 'id',
+      autoIncrement: true,
+    })
 
-      store.createIndex('gamemode', 'gamemode', { unique: false })
-      store.createIndex('isWin', 'isWin', { unique: false })
-      store.createIndex('startedAt', 'startedAt', { unique: false })
-      store.createIndex('duration', 'duration', { unique: false })
-    },
-    down(db, tx) {
-      const store = tx.objectStore(ATTEMPTS_STORE)
-
-      store.deleteIndex('gamemode')
-      store.deleteIndex('isWin')
-      store.deleteIndex('startedAt')
-      store.deleteIndex('duration')
-
-      db.deleteObjectStore(ATTEMPTS_STORE)
-    },
+    store.createIndex('gamemode', 'gamemode', { unique: false })
+    store.createIndex('isWin', 'isWin', { unique: false })
+    store.createIndex('startedAt', 'startedAt', { unique: false })
+    store.createIndex('duration', 'duration', { unique: false })
   },
 ]
 
