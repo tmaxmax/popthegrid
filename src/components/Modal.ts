25e839f0cddd50aa75d502d63a465831c545cde7
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2023-04-19 10:33:05 +0300

    Use Animated for Modal

--- src/components/Modal.ts
+++ src/components/Modal.ts
@@ -1,59 +1,19 @@
 import './Modal.css'
 
-import { Component } from './internal/Component'
+import { Animated } from './internal/Animated'
 
-class ModalContent extends Component<HTMLElement> {
+class ModalContent extends Animated<HTMLElement> {
   constructor(element: HTMLElement) {
-    super({ element, alreadyExisting: true })
+    super({ element, alreadyExisting: true, duration: { create: '1s', destroy: '0.3s' } })
     this.addClass('modal-content')
-  }
-
-  async create(parent: Component, animate: boolean): Promise<void> {
-    this.appendTo(parent)
-
-    if (animate) {
-      this.removeClass('hidden')
-      this.addClass('visible')
-      await this.waitForAnimation()
-    }
-  }
-
-  async destroy(animate: boolean): Promise<void> {
-    if (animate) {
-      this.removeClass('visible')
-      this.addClass('hidden')
-      await this.waitForAnimation()
-    }
-
-    this.remove()
   }
 }
 
-class ModalCloseButton extends Component<HTMLButtonElement> {
+class ModalCloseButton extends Animated<HTMLButtonElement> {
   constructor(onClose: () => void) {
-    super({ tag: 'button', classList: ['modal-close-button'] })
+    super({ tag: 'button', classList: ['modal-close-button'], duration: '0.1s' })
 
     this.addEventListener('click', onClose)
-  }
-
-  async create(parent: Component, animate: boolean): Promise<void> {
-    this.appendTo(parent)
-
-    if (animate) {
-      this.removeClass('hidden')
-      this.addClass('visible')
-      await this.waitForAnimation()
-    }
-  }
-
-  async destroy(animate: boolean): Promise<void> {
-    if (animate) {
-      this.removeClass('visible')
-      this.addClass('hidden')
-      await this.waitForAnimation()
-    }
-
-    this.remove()
   }
 }
 
@@ -63,60 +23,17 @@ export interface ModalProperties {
   animateClose: boolean
 }
 
-export class Modal extends Component<HTMLDivElement> {
-  private readonly content: ModalContent
-  private readonly closeButton: ModalCloseButton | undefined
-
+export class Modal extends Animated<HTMLDivElement> {
   constructor({ content, allowClose, animateClose }: ModalProperties) {
-    super({ tag: 'div', classList: ['modal'] })
+    super({ tag: 'div', classList: ['modal'], duration: { create: '2s', destroy: '0.3s' } })
 
-    this.content = new ModalContent(content)
+    this.appendChild(new ModalContent(content))
     if (allowClose) {
       const hide = function (this: Modal) {
         this.destroy(animateClose)
       }
 
-      this.closeButton = new ModalCloseButton(hide.bind(this))
+      this.appendChild(new ModalCloseButton(hide.bind(this)))
     }
-  }
-
-  async create(parent: Component, animate: boolean): Promise<void> {
-    const promiseList: Promise<void>[] = []
-    if (this.closeButton) {
-      const closeButtonPromise = this.closeButton.create(this, animate)
-      promiseList.push(closeButtonPromise)
-    }
-
-    this.appendTo(parent)
-
-    if (animate) {
-      this.removeClass('hidden')
-      this.addClass('visible')
-      promiseList.push(this.waitForAnimation())
-    }
-
-    promiseList.push(this.content.create(this, animate))
-
-    await Promise.all(promiseList)
-  }
-
-  async destroy(animate: boolean) {
-    const promiseList: Promise<void>[] = []
-    if (this.closeButton) {
-      const closeButtonPromise = this.closeButton.destroy(animate)
-      promiseList.push(closeButtonPromise)
-    }
-
-    if (animate) {
-      this.removeClass('visible')
-      this.addClass('hidden')
-      promiseList.push(this.waitForAnimation())
-    }
-
-    promiseList.push(this.content.destroy(animate))
-
-    await Promise.all(promiseList)
-
-    this.remove()
   }
 }
