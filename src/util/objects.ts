fcf2382dfd1f842bbbc9c315902a8c6c55d32281
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2023-04-25 03:06:13 +0300

    Create correctly typed Object.entries

--- src/util/objects.ts
+++ src/util/objects.ts
@@ -5,3 +5,11 @@ export function hasOwnProperty(obj: object, propKey: string): boolean {
 export function keys<T extends Record<string, unknown>>(o: T): (keyof T)[] {
   return Object.keys(o) as (keyof T)[]
 }
+
+type Entries<T> = {
+  [K in keyof T]: [K, T[K]]
+}[keyof T][]
+
+export function entries<T extends Record<string, unknown>>(o: T): Entries<T> {
+  return Object.entries(o) as Entries<T>
+}
