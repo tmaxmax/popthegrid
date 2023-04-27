8548cb198d4c73e47f54ffc28d3d8f247d7f0016
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2023-04-27 15:57:53 +0300

    remove transitions (rebase)

--- src/menu/index.ts
+++ src/menu/index.ts
@@ -9,26 +9,15 @@ export interface MenuProps {
 }
 
 export const openMenu = async ({ game, animate }: MenuProps) => {
-  let transition: Promise<void> | undefined
-
   const modal = new Modal({
     content: (target) => new Menu({ target }),
     allowClose: true,
     animateClose: animate,
     afterClose() {
-      if (transition) {
-        transition.then(() => game.resume())
-      } else {
         game.resume()
-      }
     },
   })
 
-  if (game.transition) {
-    transition = game.transition.then(() => game.pause())
-  } else {
     await game.pause()
-  }
-
   await modal.create(Component.body, animate)
 }
