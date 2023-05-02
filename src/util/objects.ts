26bbf7eb2078ddb8dab466ac305b718473c74156
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2023-05-02 17:32:47 +0300

    Allow keys util to take any object

--- src/util/objects.ts
+++ src/util/objects.ts
@@ -2,7 +2,7 @@ export function hasOwnProperty(obj: object, propKey: string): boolean {
   return Object.prototype.hasOwnProperty.call(obj, propKey)
 }
 
-export function keys<T extends Record<string, unknown>>(o: T): (keyof T)[] {
+export function keys<T extends object>(o: T): (keyof T)[] {
   return Object.keys(o) as (keyof T)[]
 }
 
