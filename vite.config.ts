c101fc850aa940951e3b0a5e4066de09219fd3f0
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2023-05-07 13:16:51 +0300

    Make app PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA as pwa } from 'vite-plugin-pwa'
 
 export default defineConfig({
   resolve: {
@@ -19,5 +20,69 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./setup-vitest.js'],
   },
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    pwa({
+      registerType: 'autoUpdate',
+      manifestFilename: 'manifest.json',
+      manifest: {
+        name: 'Pop the grid!',
+        description: 'Pop all the squares in the grid. Will you make it?',
+        icons: [
+          {
+            src: '/icons/android-icon-36x36.png',
+            sizes: '36x36',
+            type: 'image/png',
+            density: '0.75',
+          },
+          {
+            src: '/icons/android-icon-48x48.png',
+            sizes: '48x48',
+            type: 'image/png',
+            density: '1.0',
+          },
+          {
+            src: '/icons/android-icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+            density: '1.5',
+          },
+          {
+            src: '/icons/android-icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+            density: '2.0',
+          },
+          {
+            src: '/icons/android-icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            density: '3.0',
+            purpose: 'any',
+          },
+          {
+            src: '/icons/android-icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            density: '4.0',
+            purpose: 'maskable',
+          },
+          {
+            src: '/splash.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        background_color: '#000f1e',
+        theme_color: '#000f1e',
+        display: 'fullscreen',
+        start_url: '.',
+        prefer_related_applications: false,
+        id: '/',
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 })
