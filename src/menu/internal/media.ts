6b7b6893ab6bebec491651ab84e1d2084fb87232
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2023-05-09 22:34:41 +0300

    Use media state from event in createMediaMatcher

--- src/menu/internal/media.ts
+++ src/menu/internal/media.ts
@@ -3,7 +3,7 @@ import { readable } from 'svelte/store'
 export const createMediaMatcher = (query: string) => {
   const media = window.matchMedia(query)
   const store = readable(media.matches, (set) => {
-    const callback = () => set(media.matches)
+    const callback = (ev: MediaQueryListEvent) => set(ev.matches)
     media.addEventListener('change', callback)
     return () => media.removeEventListener('change', callback)
   })
